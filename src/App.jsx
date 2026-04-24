420cad22f043deade0d6142a1659581b75a79280
henai <gahaha0840@gmail.com>
2026-04-24 14:00:53 +0900

    feat: start component splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ItemsProvider } from "./context/ItemsContext";
 import Home from "./pages/Home";
-import Photos from "./pages/Photos";
 import Zukan from "./pages/Zukan";
 import Observation from "./pages/Observation";
 
 export default function App() {
   return (
-    <ItemsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/photos" element={<Photos />} />
-          <Route path="/zukan" element={<Zukan />} />
-          <Route path="/obs" element={<Observation />} />
-        </Routes>
-      </BrowserRouter>
-    </ItemsProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/zukan" element={<Zukan />} />
+        <Route path="/obs" element={<Observation />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
