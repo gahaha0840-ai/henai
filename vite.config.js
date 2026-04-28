67c3001c0d2fdb0df874a59865dd0e8e22cb8791
henai <gahaha0840@gmail.com>
2026-04-28 21:24:13 +0900

    fix:project structure

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+//Viteの挙動（開発サーバーの起動や、本番用ファイルの書き出し設定）を定義します。
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // 書き出し先のフォルダ名
+  },
 });
