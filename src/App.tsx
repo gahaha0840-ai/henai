ed6578bdfea01af7f119b16ce2b67d57d79a8076
henai <gahaha0840@gmail.com>
2026-04-28 22:54:39 +0900

    Fi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+//import Home from "./pages/Home";
+
+//function App() {
+//  return <Home />;
+//}
+
+//export default App;
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Photos from "./pages/Photos";
+import Observation from "./pages/Observation";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/photos" element={<Photos />} />
+          <Route path="/obs" element={<Observation />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
